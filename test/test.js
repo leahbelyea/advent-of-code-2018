821476a9021e364a041dd8891608fc5cf8722c9f
Leah Belyea <leahbelyea@gmail.com>
2018-12-01 09:44:58 -0400

    updated tests

--- test/test.js
+++ test/test.js
@@ -1,3 +1,25 @@
 const _ = require('lodash');
+const assert = require('assert');
+const fs = require('fs');
 const day = _.padStart(process.argv[3], 2, '0');
-require(`./${day}.js`);
+const {inputPart1, inputPart2} = require(`./testInput/${day}.js`);
+const {part1, part2} = require(`../src/${day}.js`);
+
+describe(`Running Day ${day} Tests`, function() {
+  describe('Part 1', function() {
+    _.each(inputPart1, ({input, expected}, index) => {
+      it(`should pass with sample input ${index}`, function() {
+        const answer = part1(input);
+        assert.equal(answer, expected);
+      });
+    });
+  });
+  describe('Part 2', function() {
+    _.each(inputPart2, ({input, expected}, index) => {
+      it(`should pass with sample input ${index}`, function() {
+        const answer = part2(input);
+        assert.equal(answer, expected);
+      });
+    });
+  });
+});
---
+++ test/testInput/01.js
@@ -0,0 +1,14 @@
+exports.inputPart1 = [
+  { input: ['+1', '-2', '+3', '+1'], expected: 3},
+  { input: ['+1', '+1', '+1'], expected: 3},
+  { input: ['+1', '+1', '-2'], expected: 0},
+  { input: ['-1', '-2', '-3'], expected: -6}
+];
+
+exports.inputPart2 = [
+  { input: ['+1', '-2', '+3', '+1'], expected: 2},
+  { input: ['+1', '-1'], expected: 0},
+  { input: ['+3', '+3', '+4', '-2', '-4'], expected: 10},
+  { input: ['-6', '+3', '+8', '+5', '-6'], expected: 5},
+  { input: ['+7', '+7', '-2', '-7', '-4'], expected: 14}
+];
